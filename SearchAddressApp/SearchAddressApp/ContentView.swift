03e83c4be6a2d6db4847db0c03f814a65f31a468
StanDimiroff <standimitroff@gmail.com>
2023-10-07 14:06:37 +0300

    Setup SwiftUI-based project template

---
+++ SearchAddressApp/SearchAddressApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchAddressApp
+//
+//  Created by Stanislav Dimitrov on 7.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchAddressApp/SearchAddressApp/SearchAddressAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAddressAppApp.swift
+//  SearchAddressApp
+//
+//  Created by Stanislav Dimitrov on 7.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SearchAddressAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
