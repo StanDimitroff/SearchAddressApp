e5eecd41ccbbd56d8d29909b1aefebf6a1be6b6c
StanDimiroff <standimitroff@gmail.com>
2023-10-07 14:41:23 +0300

    Create `Address` domain model

---
+++ SearchAddressApp/SearchAddressApp/Address.swift
@@ -0,0 +1,15 @@
+//
+//  Address.swift
+//  SearchAddressApp
+//
+//  Created by Stanislav Dimitrov on 7.10.23.
+//
+
+import Foundation
+
+struct Address: Identifiable {
+  let id: String
+  let label: String
+  let latitude: Double
+  let longitude: Double
+}
